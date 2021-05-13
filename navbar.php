412bd308ac9615e0170865bca0c038b4c2bd2b21
uyanga0212 <little_harry0212@yahoo.com>
2021-05-14 05:53:51 +0800

    update

--- navbar.php
+++ navbar.php
@@ -26,6 +26,9 @@
             <li class="nav-item">
               <a class="nav-link" href="/register.php">Register</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/logout.php">Logout</a>
+            </li>
         </ul>
         </div>
     </nav>
